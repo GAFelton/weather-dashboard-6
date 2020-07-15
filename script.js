5bdc5bc5649f443e2432f0f1325f76f00389f7e6
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-15 14:08:57 -0700

    Added pseudocode for scripts

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ var previousSearchesArea = $("#previousSearchesArea");
 var currentCityHeader = $("currentCityHeader");
 var weatherDetailsArea = $("weatherDetailsArea");
 var fiveDayForecastArea = $("fiveDayForecastArea");
+var weatherAPIKey = config.MY_KEY;
 
 function weatherData() {
     var thisSearch = searchBar.val().trim();
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + thisSearch + "&appid=" + config.MY_KEY;
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + thisSearch + "&appid=" + weatherAPIKey;
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -15,3 +16,19 @@ function weatherData() {
 
     });
 }
+
+
+/* PSEUDOCODE:
+-weatherData api call function
+-5day forecast api call function
+-search bar event listener (
+    takes userInput and runs both api calls. 
+    Adds userInput to localStorage.
+)
+-previous searches function (getItem from localStorage)
+-localStorage setItem array of previous searches
+-onPageLoad if there are previous searches in localStorage, use the most recent one to run both api calls
+
+*BONUS* onPageLoad if there are no previous searches in localStorage, use the users current localtion to run both api calls.
+
+*/
