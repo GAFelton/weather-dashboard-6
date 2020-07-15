9bd10e2e56fbc443b4f8d65f195a429a68d1b3d3
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-15 10:24:40 -0700

    Built & commented basic page structure. Add ID variables to script.js.

---
+++ script.js
@@ -0,0 +1,6 @@
+var searchArea = $("#searchArea");
+var searchBar = $("#searchBar");
+var previousSearchesArea = $("#previousSearchesArea");
+var currentCityHeader = $("currentCityHeader");
+var weatherDetailsArea = $("weatherDetailsArea");
+var fiveDayForecastArea = $("fiveDayForecastArea");
